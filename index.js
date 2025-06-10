f9ed1822fd656c49047d31d02de5f1614cf527a2
kristenlee <leekristen090@gmail.com>
2025-06-10 12:44:16 -0400

    hopefully fix

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const app = express();
 app.use(
     cors({
              credentials: true,
-             origin: process.env.NETLIFY_URL || "http://localhost:5173",
+             origin: [
+                 process.env.NETLIFY_URL,
+                 "http://localhost:5173",
+                 "https://kambaz-react-web-app-leekri.netlify.app/" // Add your actual Netlify URL here
+             ]
+             // origin: process.env.NETLIFY_URL || "http://localhost:5173",
              // origin: process.env.NETLIFY_URL,
          })
 );
