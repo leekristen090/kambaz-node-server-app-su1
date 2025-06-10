1732c5c4d78884dbc000eafeb0227c098d042213
kristenlee <leekristen090@gmail.com>
2025-06-10 12:26:29 -0400

    edits

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -83,4 +83,5 @@ export default function UserRoutes(app) {
     app.post("/api/users/profile", profile);
     app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
     app.post("/api/users/current/courses", createCourse)
+
 }
--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const app = express();
 app.use(
     cors({
              credentials: true,
-             // origin: process.env.NETLIFY_URL || "http://localhost:5173",
-             origin: process.env.NETLIFY_URL,
+             origin: process.env.NETLIFY_URL || "http://localhost:5173",
+             // origin: process.env.NETLIFY_URL,
          })
 );
 const sessionOptions = {
