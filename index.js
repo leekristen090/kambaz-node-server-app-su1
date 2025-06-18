b62276e8241cbf096dd138c40b42b4cb130de983
kristenlee <leekristen090@gmail.com>
2025-06-17 22:07:05 -0400

    small edit

--- index.js
+++ index.js
@@ -15,13 +15,7 @@ const app = express();
 app.use(
     cors({
              credentials: true,
-             // origin: [
-             //     process.env.NETLIFY_URL,
-             //     "http://localhost:5173",
-             //     "https://kambaz-react-web-app-leekri.netlify.app/"
-             // ]
              origin: process.env.NETLIFY_URL || "http://localhost:5173",
-             // origin: process.env.NETLIFY_URL,
          })
 );
 const sessionOptions = {
